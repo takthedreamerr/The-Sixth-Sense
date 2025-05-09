af6169b1ef061b7f4f67545e67789ca78daaf4d0
Naledi-Sithole05 <justnalz123@gmail.com>
2025-05-09 10:29:08 +0200

    life

---
+++ Assets/Scripts/Dialouge.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Dialouge : MonoBehaviour
+{ 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [System.Serializable]
+    public class Dialogue
+    {
+        public string name;
+        public string[] sentences;
+    }
+
+
+
+
+
+}
