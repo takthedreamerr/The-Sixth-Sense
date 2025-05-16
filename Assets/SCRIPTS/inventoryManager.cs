464aed5052e1a5183156b0cfe6b1002033f023f1
olesego2 <2703178@students.wits.ac.za>
2025-05-16 10:05:19 +0200

    inventory system

--- Assets/SCRIPTS/inventoryManager.cs
+++ Assets/SCRIPTS/inventoryManager.cs
@@ -6,7 +6,8 @@ public class InventoryManager : MonoBehaviour
 {
     public GameObject InventoryMenu;
     private bool menuActivated = false;
-
+    public ItemSlot[] itemSlot;
+ 
     // Update is called once per frame
     void Update()
     {
@@ -26,5 +27,23 @@ public class InventoryManager : MonoBehaviour
         menuActivated = !menuActivated;
         InventoryMenu.SetActive(menuActivated);
     }
+
+ 
+     
 }
+    
+    public void AddItem(string itemName, int quantity, Sprite itemSprite)
+{
+    for (int i = 0; i < itemSlot.Length; i++)
+    {
+        if (!itemSlot[i].isFull)
+        {
+            itemSlot[i].AddItem(itemName, quantity, itemSprite);
+            return;
+        }
+    }
+
+    Debug.LogWarning("Inventory is full. Could not add item: " + itemName);
+}
+
 }
