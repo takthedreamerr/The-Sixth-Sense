d0206e136d3992a2b949aea908fa110b4b9dbe90
Naledi-Sithole05 <justnalz123@gmail.com>
2025-05-16 09:55:07 +0200

    final selection

---
+++ Assets/Scripts/LoadSceneButton.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+
+public class LoadSceneButton: MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(3);
+    }
+}
+
